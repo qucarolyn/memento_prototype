2e58450dcbfd2e32083da21d004ffa5993f60818
Graciela <graciela.smet@gmail.com>
2021-03-10 23:14:47 -0800

    reflect screen styling

--- App/Screens/ReflectScreen.js
+++ App/Screens/ReflectScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, KeyboardAvoidingView, TextInput, StyleSheet } from "react-native";
+import { View, Text, Button, TouchableOpacity, KeyboardAvoidingView, TextInput, StyleSheet } from "react-native";
 
 export default function ReflectScreen({navigation}) {
     return (
@@ -8,14 +8,22 @@ export default function ReflectScreen({navigation}) {
           <View>
             <Text style={{margin: 10,}}>What have you enjoyed most about the last week?</Text>
             <TouchableOpacity style={{alignItems: 'center'}}>
-              <Text style={{color: 'blue', textDecorationLine: 'underline'}}>I want a new prompt</Text>
+              <Text style={styles.newprompt}>I want a new prompt</Text>
             </TouchableOpacity>
           </View>
-          <KeyboardAvoidingView style={{alignItems: 'center', margin: 50,}}>
+          <KeyboardAvoidingView style={{alignItems: 'center', margin: 20,}}>
             <TextInput
+              multiline
               style={styles.textinput}
             />
           </KeyboardAvoidingView>
+          <TouchableOpacity style={styles.voicememo}>
+            <Text>Record a voice memo</Text>
+          </TouchableOpacity>
+          <Button
+            title="Save"
+            accessibilityLabel="Click Here to Publish Vision"
+          />
         </View>
     );
 }
@@ -23,10 +31,22 @@ export default function ReflectScreen({navigation}) {
 const styles = StyleSheet.create({
   textinput: {
     padding: 15,
-    height: 36,
-    width: '70%',
+    paddingTop: 15,
+    height: 300,
+    width: 300,
+    alignItems: 'flex-start',
     borderColor: 'lightgray',
     borderWidth: 1,
     borderRadius: 18,
+  },
+  voicememo: {
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    padding: 10,
+    marginBottom: 30,
+  },
+  newprompt: {
+    color: 'blue',
+    textDecorationLine: 'underline',
   },
 });
