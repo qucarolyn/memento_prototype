34759f93262b06add0e5e020f6b259638443ad9f
qucarolyn <qu.carolyn@gmail.com>
2021-03-10 20:58:28 -0800

    homescreen (partial)

---
+++ App/Screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View, SafeAreaView, Image, Dimensions } from "react-native";
+
+
+
+export default function HomeScreen() {
+
+    return(
+        <View> 
+            <Text>
+                Home Screen
+            </Text>
+        </View>
+
+    );
+
+}
