c71b433117c3205baa8bdfa19f94fe2e4d3376b5
Graciela <graciela.smet@gmail.com>
2021-03-10 22:20:50 -0800

    vision add screen

--- App/Screens/VisionAddScreen.js
+++ App/Screens/VisionAddScreen.js
@@ -1,13 +1,45 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 
 export default function HomeFeedScreen({navigation}) {
     return (
         <View>
+          <View>
+            <Text>Enter a title</Text>
+          </View>
+          <View>
+            <Text>Choose a color</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center',}}>
+            <TouchableOpacity style={{
+              height: 30, width: 30, borderRadius: 15, margin: 5,
+              backgroundColor: 'red',
+            }}/>
+            <TouchableOpacity style={{
+              height: 30, width: 30, borderRadius: 15, margin: 5,
+              backgroundColor: 'orange',
+            }}/>
+            <TouchableOpacity style={{
+              height: 30, width: 30, borderRadius: 15, margin: 5,
+              backgroundColor: 'yellow',
+            }}/>
+            <TouchableOpacity style={{
+              height: 30, width: 30, borderRadius: 15, margin: 5,
+              backgroundColor: 'green',
+            }}/>
+            <TouchableOpacity style={{
+              height: 30, width: 30, borderRadius: 15, margin: 5,
+              backgroundColor: 'blue',
+            }}/>
+            <TouchableOpacity style={{
+              height: 30, width: 30, borderRadius: 15, margin: 5,
+              backgroundColor: 'purple',
+            }}/>
+            </View>
+          </View>
 
           <Button
-            title="Click Here to Create a Vision"
-            accessibilityLabel="Click Here to Create a Vision"
+            title="Finish"
+            accessibilityLabel="Click Here to Publish Vision"
           />
         </View>
     );
