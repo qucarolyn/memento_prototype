eb1278cdf0dd0a8c16913d57cb6e922fe415b828
Graciela <graciela.smet@gmail.com>
2021-03-10 22:48:40 -0800

    reflect screen initial styling

--- App/Screens/ReflectScreen.js
+++ App/Screens/ReflectScreen.js
@@ -4,9 +4,17 @@ import { View, Text } from "react-native";
 export default function ReflectScreen({navigation}) {
     return (
         <View>
-          <Text>
-              Refelct
-          </Text>
+          <Text>Reflect</Text>
+          <Text>What have you enjoyed most about the last week?></Text>
+          <TouchableOpacity>
+            <Text>I want a new prompt</Text>
+          </TouchableOpacity>
+          <KeyboardAvoidingView style={{alignItems: 'center', margin: 50,}}>
+            <TextInput
+              style={styles.textinput}
+              onChangeText={(text) => setSearchText(text)}
+            />
+          </KeyboardAvoidingView>
         </View>
     );
 }
