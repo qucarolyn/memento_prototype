67d7768df1b0a7468d9751660ee94c706425dd04
qucarolyn <qu.carolyn@gmail.com>
2021-03-10 20:46:42 -0800

    update

--- App.js
+++ App.js
@@ -3,73 +3,16 @@ import { StyleSheet, SafeAreaView , View, Text} from "react-native";
 import { Button } from 'react-native-elements';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import HomeScreen from "./App/Components/HomeScreen";
-import ArticlePage from "./App/Components/ArticlePage";
 
 const Stack = createStackNavigator();
       
 export default function App() {
-  const [firstLaunch, setFirstLaunch] = useState(true);
-
-  const checkOnboarding = async () => {
-    try{
-      const onboarded = await AsyncStorage.getItem('onboarded');
-      if(onboarded !== null && onboarded === 'true') {
-      }else { 
-        AsyncStorage.setItem('onboarded', 'true');
-      }
-
-    }catch(e) {
-      console.error(e);
-    }
-  }
-
-  const renderHomePage = () => {
-      return <NavigationContainer>
+  return (
+    <NavigationContainer>
        <Stack.Navigator>
          <Stack.Screen name="Home" component={HomeScreen} />
-         <Stack.Screen 
-           name="Article" 
-           component={ArticlePage} 
-         />
        </Stack.Navigator>
       </NavigationContainer>
-
-  }
-
-  const renderOnboard =  () => {
-    return <SafeAreaView style = {styles.welcomeView}>
-      <Text style = {styles.welcomeText}>Welcome to NYT!</Text>
-      <View style = {styles.captionView}>
-        <Text style = {styles.captionText}>It looks like this is your first time using the app.</Text>
-        <Text style = {styles.captionText}>Please press the button below to continue</Text>
-      </View>
-      
-      <Button
-          title = "Open the App"
-          raised = {true}
-          onPress = {() => {
-            checkOnboarding();
-            setFirstLaunch(false);
-          }}
-      />
-    </SafeAreaView>
-}
-
-  const renderPage = () => {
-    checkOnboarding();
-    if(firstLaunch === true){
-      return renderOnboard();
-    }else{
-      return renderHomePage();
-    }
-
-  }
-
-  return (
-    renderPage()
   );
 }
 
