68b696325bec5f6eccec28f679a2775136397ffb
Graciela <graciela.smet@gmail.com>
2021-03-15 12:48:11 -0700

    MementoAddScreen

--- App/Screens/MementoAddScreen.js
+++ App/Screens/MementoAddScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function HomeFeedScreen({navigation}) {
+export default function MementoAddScreen({navigation}) {
     return (
         <View>
           <Text>
