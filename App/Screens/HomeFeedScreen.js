acb4c793e8637458ee45b0fcdcf91fb1eff7bf29
Graciela <graciela.smet@gmail.com>
2021-03-16 13:03:52 -0700

    more styling

--- App/Screens/HomeFeedScreen.js
+++ App/Screens/HomeFeedScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button, SafeAreaView, TouchableOpacity} from "react-native";
+import { View, Text, Button, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView} from "react-native";
 import HorizontalMenu from '../Components/HorizontalMenu.js';
 import MementoFeed from '../Components/MementoFeed.js';
 // import TripleToggleSwitch from 'react-triple-toggle-switch';
@@ -60,29 +60,48 @@ export default function HomeFeedScreen({navigation}) {
           </View>
 
           {/* <Text>{console.log(visions)}</Text> */}
-          <SafeAreaView>
+          <ScrollView style={{paddingBottom: 100}}>
+
+          <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("MementoAdd", {currentVision: activeVision, visions: visions})}
+              style={styles.button}
+            >
+            <Text style={{color: 'white'}}>Add a Memento</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Reflect")}
+              style={styles.button}
+            >
+              <Text style={{color: 'white'}}>Add a Reflection</Text>
+            </TouchableOpacity>
+
+          </View>
+
             <MementoFeed
               vision = {activeVision}//for testing purposes
             ></MementoFeed>
 
-            <Button
-              onPress={() => navigation.navigate("MementoAdd", {currentVision: activeVision, visions: visions})}
-              title="Create a Memento"
-            />
+          <View style={{height: 150}}/>
 
-            <Button
-              onPress={() => navigation.navigate("VisionAdd", {updateVision: {addVision}})}
-              title="Create a Vision"
-            />
-
-            <Button
-              onPress={() => navigation.navigate("Reflect")}
-              title="Reflect"
-            />
-
-          </SafeAreaView>
+          </ScrollView>
 
 
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+      backgroundColor: '#3E71AE',
+      borderRadius: 15,
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 30,
+      width: 150,
+      padding: 5,
+      margin: 5,
+    }
+  });
