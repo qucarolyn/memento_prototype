c43bb46ddc09cffae2a11b96d349ca9113b5edeb
qucarolyn <qu.carolyn@gmail.com>
2021-03-17 13:21:37 -0700

    add reflection flow

--- App/Components/MementoFeed.js
+++ App/Components/MementoFeed.js
@@ -9,89 +9,8 @@ import ReflectionThumbnail from './ReflectionThumbnail.js';
 
 export default function MementoFeed(props) {
     let navigation = useNavigation();
-    const MementosAndReflections = [
-      {
-        title: "Stay healthy",
-        reflection: false,
-        color: '#FFAD80',
-        date: "1/21/2020",
-        caption: 'today I went on a run! Was very very fun. Im trying to max out the character count but im not sure if it will work lets see how this goes!',
-        favorite: true,
-      },
-      {
-        title: "Learn ukelele",
-        reflection: false,
-        color: '#83E39E',
-        date: "1/19/2020",
-        caption: 'Learned a brand new chord progression!',
-        favorite: true,
-      },
-      {
-        title: "Stay healthy",
-        reflection: true,
-        color: '#FFAD80',
-        date: "1/19/2020",
-        prompt: "What are you most proud of?",
-        caption: 'I am most proud of myself for keeping up with this vision consistently',
-        favorite: false,
-      },
-
-      {
-        title: "Spend time with fam",
-        reflection: false,
-        color: '#80C9FF',
-        date: "1/19/2020",
-        caption: 'Got lunch with mom and dad today! it was SO SO SO much fun! ',
-        favorite: false,
-      },
-
-      {
-        title: "Spend time with fam",
-        reflection: false,
-        color: '#80C9FF',
-        date: "1/19/2020",
-        caption: 'Family game night yeah-yuh!!! ',
-        favorite: false,
-      },
-
-      {
-        title: "Learn ukelele",
-        reflection: false,
-        color: '#83E39E',
-        date: "1/19/2020",
-        caption: 'Played in the dorm with my RAs Sarah and Theo today! turns out they also have been trying to learn!',
-        favorite: true,
-      },
-
-      {
-        title: "Learn ukelele",
-        reflection: true,
-        color: '#83E39E',
-        date: "1/19/2020",
-        prompt: "What are you most proud of?",
-        caption: 'I am most proud of myself for keeping up with this vision consistently',
-        favorite: true,
-      },
-
-      {
-        title: "Spend time with fam",
-        reflection: false,
-        color: '#80C9FF',
-        date: "1/19/2020",
-        caption: 'Filler',
-        favorite: false,
-      },
-
-      {
-        title: "Spend time with fam",
-        reflection: false,
-        color: '#80C9FF',
-        date: "1/19/2020",
-        caption: 'Filler',
-        favorite: false,
-      },
-
-    ];
+    let feedItems = props.feedItems
+    console.log(feedItems)
     
     function shortenText (text){
       if(text.length <= 50) {
@@ -144,7 +63,7 @@ export default function MementoFeed(props) {
 
     return (
       <FlatList
-      data = {filterMementos(MementosAndReflections, props.vision)}//need to filter mementos based on the click (callback function)
+      data = {filterMementos(feedItems, props.vision)}//need to filter mementos based on the click (callback function)
       renderItem = {({item}) => {
         if(item.reflection == false){
           return (
