14a887cdcf0f7348fe5aa691c2b0cd2ef3f4a115
Graciela <graciela.smet@gmail.com>
2021-03-17 19:09:11 -0700

    help garbage

--- App/Screens/HelpScreen.js
+++ App/Screens/HelpScreen.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button, SafeAreaView, TouchableOpacity} from "react-native";
+import { View, Text, Button, SafeAreaView, TouchableOpacity, StyleSheet} from "react-native";
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
 
 export default function DrawerScreen({route, navigation}) {
     return(
-    <View>
-        <Text>
+    <View style={styles.container}>
+        <Text style={styles.help}>
             Help
         </Text>
     </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#3E71AE',
+      borderRadius: 15,
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 30,
+      width: 150,
+      padding: 5,
+      margin: 5,
+    },
+    help: {
+      color: 'white',
+      fontFamily: 'Futura',
+      fontSize: 20,
+    }
+  });
